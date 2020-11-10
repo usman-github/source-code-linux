211b570299b5a62db3b6ee2f5bf601a82c41fa06
Test <testmail@testmail.com>
2020-11-10 08:07:15 +0000

    newbranch

--- src/main/java/MessageUtil.java
+++ src/main/java/MessageUtil.java
@@ -11,7 +11,7 @@ public class MessageUtil {
   }
 
   public String salutationMessage() {
-    message = "Hi!" + message;
+    message = "Hi-new!" + message;
     System.out.println(message);
     return message;
   }
